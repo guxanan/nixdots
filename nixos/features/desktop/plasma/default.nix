86edb619711a941034713b78fa6029b87dfbe202
Gheorghiu Alin-Adrian <anghu.adrian@gmail.com>
2024-09-28 21:22:40 +0200

    enabling wayland for sddm

--- nixos/features/desktop/plasma/default.nix
+++ nixos/features/desktop/plasma/default.nix
@@ -6,7 +6,10 @@
 }: {
   config = lib.mkIf config.settings.desktop.plasma.enable {
     services = {
-      displayManager.sddm.enable = true;
+      displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
       desktopManager.plasma6.enable = true;
     };
   };
